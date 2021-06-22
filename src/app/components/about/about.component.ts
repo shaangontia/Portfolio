d62d07743ed239ed89137a31260debc8f1d6e292
Shaan <shaan.ce@gmail.com>
2021-06-23 00:25:41 +0530

    Refactored age calculation

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,17 +7,24 @@ import {Component} from '@angular/core';
 })
 export class AboutComponent {
 
+  getAge() {
+    const birthdate = new Date("1989/08/21");
+    const cur = new Date();
+    const diff = cur - birthdate;
+    return Math.floor(diff / 31557600000);
+  }
+
   constructor() { }
   about = {
     name: 'Shantanu Gontia',
     address: 'Bengaluru',
-    age: '30',
+    age: this.getAge(),
     email: 'shaan.ce@gmail.com',
     contact: '+91-8867635500',
-    description: 'Hello! I am <strong>Shantanu Gontia </strong>. Web Developer with over 6\n' +
+    description: 'Hello! I am <strong>Shantanu Gontia </strong>. Web Developer with over 8\n' +
       '          years of experience. Experienced with all stages of the development\n' +
-      '          cycle for dynamic web projects. Having an in-depth knowledge including\n' +
-      '          advanced <strong>HTML5, CSS3, JavaScript, jQuery, AngularJS, Angular and Swift</strong>.\n' +
+      '          cycle for dynamic web projects. I have an expertise in \n' +
+      '          <strong>HTML5, CSS3, JavaScript, Typescript, React, AngularJS, Angular and Swift</strong>.\n' +
       '          I aim to make a difference through my creative solution.'
   };
 }
